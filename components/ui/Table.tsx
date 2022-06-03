7b887f22dae82292088c4c0cee293d938ef13520
Pedro Fuenmayor <pedropfuenmayor@gmail.com>
2022-06-03 08:35:05 +0200

    responsive logs

--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -5,8 +5,8 @@ export default function Table() {
     <div className="relative overflow-x-auto mt-8">
       <table className="w-72 text-[10px] text-left m-auto sm:text-sm sm:w-96">
         <tbody className="">
-          {activityAlertLogData.map((log) => (
-            <tr className="bg-white flex justify-between">
+          {activityAlertLogData.map((log, i) => (
+            <tr key={i} className="bg-white flex justify-between">
               <td className="w-[89px] sm:w-[120px]">{log.timestamp}</td>
               <td className="w-[18px]">
                 <log.icon className="h-4 w-4" />
